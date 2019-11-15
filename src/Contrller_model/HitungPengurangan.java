dfad506e9a6fe1d7541f64a949b899f667338d5a
Christoporus Chiesa Yuarsa-K3518018 <>
2019-11-15 20:37:34 +0700

    Christoporus Chiesa Yuarsa-K3518018

---
+++ src/Contrller_model/HitungPengurangan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Contrller_model;
+
+/**
+ *
+ * @author Christoporus Chiesa Yuarsa
+ */
+//inheritance
+public class HitungPengurangan extends HitungPenjumlahan{
+    //overriding
+    @Override
+     public int hitung(int a, int b){
+        return a-b;
+    }
+}
