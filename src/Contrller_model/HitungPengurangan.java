7b813ebf97732e9ae0939472377601b583686824
chiesayuarsa <57786685+chiesayuarsa@users.noreply.github.com>
2019-11-16 13:02:16 +0700

    Christoporus Chiesa Yuarsa - K3518018

--- src/Contrller_model/HitungPengurangan.java
+++ src/Contrller_model/HitungPengurangan.java
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package Contrller_model;
 
 /**
  *
  * @author Christoporus Chiesa Yuarsa
  */
+
 //inheritance
 public class HitungPengurangan extends HitungPenjumlahan{
     //overriding
